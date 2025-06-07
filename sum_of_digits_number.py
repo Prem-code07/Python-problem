ee8a0f57346b2fda5bdaeca5be8ad2178da25b2e
Pream code <preamanandasaint99@gmail.com>
2025-06-07 23:16:28 +0530

    It's all are siome basic problem .

--- sum_of_digits_number.py
+++ sum_of_digits_number.py
@@ -1,16 +1,7 @@
-# Python program to calculate the sum of digits of a number
-
-# Get user input
 num = int(input("Enter a number: "))
-
-# Initialize sum
 sum_of_digits = 0
-
-# Loop to extract digits and compute the sum
 while num > 0:
     digit = num % 10
     sum_of_digits += digit
     num = num // 10
-
-# Display the result
 print("Sum of digits:", sum_of_digits)
