51f03e182c1d35241343d1b11aa3da8ed7f76d3f
Pream code <preamanandasaint99@gmail.com>
2025-06-08 19:07:54 +0530

    It's an simpule python program.

--- qube_root_numbner.py
+++ qube_root_numbner.py
@@ -1,8 +1,3 @@
-# Python program to find the cube root of a number
-
 num = float(input("Enter a number: "))
-
-# Calculate cube root
 cube_root = num ** (1/3)
-
 print("Cube root of", num, "is", cube_root)
